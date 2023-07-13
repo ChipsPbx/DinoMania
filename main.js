650dce1ec9c7314087bb66f2469000c80f36b879
Thomas Dutouquet <thomas.dutouquet@live.fr>
2023-07-13 19:21:50 +0200

    Added CGV page and text-pages styles

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// Header & footer
+
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        // Insérer HTML ici
+        // HTML header ici
         `
     }
 }
@@ -9,10 +11,23 @@ class Header extends HTMLElement {
 class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        // Insérer HTML ici
+        // HTML footer ici
         `
     }
 }
 
 customElements.define('main-header', Header);
 customElements.define('main-footer', Footer);
+
+// Get to top button
+
+let getToTop = document.getElementById("backToTop");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        getToTop.style.display = "block";
+    } else {
+        getToTop.style.display = "none";
+    }
+}
