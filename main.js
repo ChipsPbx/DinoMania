74132983a47a72945cbbc530abcd67c1d7e61098
Thomas Dutouquet <thomas.dutouquet@live.fr>
2023-07-17 19:40:45 +0200

    Ajout header

--- main.js
+++ main.js
@@ -3,7 +3,32 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        // HTML header ici
+        <header id="mainHeader">
+        <figure id="logo" class="headerBlock">
+            <a href="./index.html">
+                <img src="./images/logo-dinomania-w.png" alt="logo" id="dinomania" />
+            </a>
+        </figure>
+        <form action="" class="headerBlock">
+            <input type="search" name="search" id="search" placeholder="Trouvez le dino de vos rêves..." />
+        </form>
+        <figure id="icon" class="headerBlock">
+            <a href="">
+                <img src="./icons/Icon_profil.png" alt="login" />
+            </a>
+            <a href="">
+                <img src="./icons/panier.png" alt="shop" />
+            </a>
+        </figure>
+        </header>
+        <nav id="mainNav">
+                <ul>
+                    <li><a href="#">Nouveautés</a></li>
+                    <li><a href="/cat-dinos.html">Dinosaures</a></li>
+                    <li><a href="/cat-accessories.html">Accessoires</a></li>
+                    <li><a href="/cat-food.html">Nourriture</a></li>
+                </ul>
+        </nav>
         `
     }
 }
